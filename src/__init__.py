473e78038cedc148267d24ac9cff61113e00c4e1
Anthony Ugwu <anthonyugwu234@gmail.com>
2021-07-04 02:13:26 +0100

    setup postgres database

--- src/__init__.py
+++ src/__init__.py
@@ -2,19 +2,16 @@ from flask import Flask, jsonify
 from flask_restx import Resource, Api
 
 
-
 app = Flask(__name__)
 
 api = Api(app)
 
-app.config.from_object('src.config.DevelopmentConfig')
+app.config.from_object("src.config.DevelopmentConfig")
+
 
 class Test(Resource):
     def get(self):
-        return {
-            'status': 'success',
-            'message': 'test!'
-        }
+        return {"status": "success", "message": "test!"}
 
 
-api.add_resource(Ping, '/test')
+api.add_resource(Test, "/test")
